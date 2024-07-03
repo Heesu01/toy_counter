1796e5a6d8610438a399f69e60926da3d467c67a
김희수 <heesu01@gimhuisuui-noteubug.local>
2024-07-04 00:47:34 +0900

    useEffect사용

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Viewer from "./components/Viewer";
 import Controller from "./components/Controller";
+import Even from "./components/Even";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
   const handleSetCount = (value) => {
     setCount(count + value);
   };
+  const handleChangeText = (e) => {
+    setText(e.target.value);
+  };
+
+  const didMountRef = useRef(false);
+  // useEffect(() => {
+  //   console.log("text, count 업데이트: ", text, count);
+  // }, [text, count]);
+  useEffect(() => {
+    if (!didMountRef.current) {
+      didMountRef.current = true;
+      return;
+    } else {
+      console.log("컴포넌트 업데이트! ");
+    }
+  });
+  useEffect(() => {
+    console.log("컴포넌트 마운트");
+  }, []);
   return (
     <div className="App">
       <h1>Simple Counter</h1>
       <section>
+        <input value={text} onChange={handleChangeText} />
+      </section>
+      <section>
         <Viewer count={count} />
+        {count % 2 === 0 && <Even />}
       </section>
       <section>
         <Controller handleSetCount={handleSetCount} />
